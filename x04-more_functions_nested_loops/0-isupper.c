01add75efbd1b1852d0696035cc2cb862953db99
Benlem <zoubidabenlem@gmail.com>
2023-03-24 16:52:51 -0700

    function is upper

---
+++ x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always returns 0
+ *
+ */
+int main(void)
+{
+	int _isupper(int c){
+        if(c >='A' && c <='Z'){
+            return (1);
+        }
+        else{
+            return (0);
+        }
+    }
+	return (0);
+}
