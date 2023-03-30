a944bf6b26047a4a7e5038687f7d6791d0ac20eb
Benlem <zoubidabenlem@gmail.com>
2023-03-30 15:31:11 -0700

    prototype

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _strcat - concats strings
+ * @dest: destination of string
+ * @src: source of string to concat
+ *
+ * Return: returns char value
+ */
+char *_strcat(char *dest, char *src);
+#endif
