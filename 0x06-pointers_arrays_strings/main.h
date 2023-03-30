773a1fe5cb5697a4da6a580f72e143128475a43e
Benlem <zoubidabenlem@gmail.com>
2023-03-30 15:42:37 -0700

    strncat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -8,4 +8,12 @@
  * Return: returns char value
  */
 char *_strcat(char *dest, char *src);
+/**
+ * _strncat - concat string at n times
+ * @dest: destination source
+ * @src: source of array
+ * @n: n amount of times
+ * Return: return char value
+ */
+char *_strncat(char *dest, char *src, int n);
 #endif
