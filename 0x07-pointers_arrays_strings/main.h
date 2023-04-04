66f0f73c1601ecd3a81259ed7182110e798f7c06
Benlem <zoubidabenlem@gmail.com>
2023-04-03 18:26:23 -0700

    main

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _memset - set memory to a character
+ * @s: value and pointer
+ * @b: value to replace at pointers
+ * @n: n amount of times to replace
+ * Return: returns a pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n);
