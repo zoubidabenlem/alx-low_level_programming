4144379cdffa2151f747edb3dead15dcac8dc14c
Benlem <zoubidabenlem@gmail.com>
2023-04-03 20:08:26 -0700

    prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -50,4 +50,10 @@ char *_strstr(char *haystack, char *needle);
  *
  */
 void print_chessboard(char (*a)[8]);
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
 #endif
