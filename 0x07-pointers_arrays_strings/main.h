3a451cfa86a7d9d797f2ef08ac3e606e869abca1
Benlem <zoubidabenlem@gmail.com>
2023-04-03 19:15:27 -0700

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -8,4 +8,12 @@
  * Return: returns a pointer to s
  */
 char *_memset(char *s, char b, unsigned int n);
+/**
+ * _memcpy - copies value from src to dest
+ * @dest: destination to copy
+ * @src: source to copy from
+ * @n: amount of times from 0 to copy
+ * Return: return string char value
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
