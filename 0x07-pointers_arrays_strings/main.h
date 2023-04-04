9060f5fa0a35f7f2ae93ea16f50fb7aad6a6836c
Benlem <zoubidabenlem@gmail.com>
2023-04-03 19:30:23 -0700

    main

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -16,4 +16,11 @@ char *_memset(char *s, char b, unsigned int n);
  * Return: return string char value
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
+/**
+ * _strchr - locates character in string
+ * @s: string to find character
+ * @c: character to find
+ * Return: found character
+ */
+char *_strchr(char *s, char c);
 #endif
