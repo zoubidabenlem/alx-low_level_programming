5a950b1eef61486a91446fa34a37373fcf88de13
Benlem <zoubidabenlem@gmail.com>
2023-04-03 18:31:35 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,11 +6,13 @@
  * @n: n amount of times
  * Return: char value of s
  */
-
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
-    for(i = 0 ; i < n; i++){
-        s[i] = b;
-    }
-    return (s);
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
