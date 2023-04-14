91ed866dd2d7b2b757682016b08114f1eae461b6
Benlem <zoubidabenlem@gmail.com>
2023-04-14 11:02:34 -0700

    how about not

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,12 @@
  * Return: something void or pointer
  */
 void *malloc_checked(unsigned int b);
+/**
+ * string_nconcat - concats strings
+ * @s1: string one
+ * @s2: string two
+ * @n: n amount of bytes of s2
+ * Return: a char pointer value
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 #endif
