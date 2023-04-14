2a5dffade18cfe7cd076b27878670b7103a96354
Benlem <zoubidabenlem@gmail.com>
2023-04-14 10:53:03 -0700

    check

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,8 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * malloc_checked - checks malloc?
+ * @b: int to check? something
+ * Return: something void or pointer
+ */
+void *malloc_checkedvoid *malloc_checked(unsigned int b);
