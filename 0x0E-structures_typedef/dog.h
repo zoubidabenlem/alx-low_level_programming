36f0b9613a824a0d33c51ac9dff250994723c386
Benlem <zoubidabenlem@gmail.com>
2023-04-17 15:43:32 -0700

    free dog obj

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,6 +13,7 @@ struct dog
 	char *owner;
 };
 typedef struct dog dog_t;
+void free_dog(dog_t *d);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
