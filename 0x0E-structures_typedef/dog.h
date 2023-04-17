fac940eb0f8c293b8ed4181ea3e4acc686d8c47f
Benlem <zoubidabenlem@gmail.com>
2023-04-17 14:30:02 -0700

    added readme and dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - structure of dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
