833148fbdcd9143afd834155691bec4e2a4a1ee3
Benlem <zoubidabenlem@gmail.com>
2023-04-19 15:33:55 -0700

    itterate array

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name: prints name
+ * print_name - print the name
  * @name: name to print
  * @f: function to use
-*/
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
-		f(name);
-}
+		
---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array itterator: iterates array
+ * @array: array to iterate
+ * @size: size of an array
+ * @action: action to perform
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+	for(i = 0; i <size; i++)
+	{
+		action(array[i]);
+	}
+}
