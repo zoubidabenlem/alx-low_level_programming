f2108396ca5cf0bb623a0199a125b91766a5dd6d
Benlem <zoubidabenlem@gmail.com>
2023-04-19 16:51:57 -0700

    fixed error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -7,15 +7,16 @@
  * @size: number of elements in the array
  * @cmp: a pointer to the function to be used to compare values
 */
-int int_index(int *array, int size, int (*cmp)(int))
+int int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	if(array == NULL || size <= 0 || cmp == NULL)
+
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
-	for(i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		if(cmp(array[i]))
-			return (i);
+			if (cmp(array[i]))
+				return (i);
 	}
-return (-1);
+	return (-1);
 }
