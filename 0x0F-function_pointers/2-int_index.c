8c956109e8fcd3b3a83a1fce9c890699fc04f300
Benlem <zoubidabenlem@gmail.com>
2023-04-19 16:48:52 -0700

    fixed error

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,20 +10,12 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	int b=0;
-	if(array == NULL)
-		exit(98);
-	if(size <= 0)
+	if(array == NULL || size <= 0 || cmp == NULL)
 		return (-1);
 	for(i = 0; i < size; i++)
 	{
 		if(cmp(array[i])==0)
-		{
-			b=1;
 			return (i);
-		}
 	}
-	if(b == 0)
-		return (-1);
-return (i);
+return (-1);
 }
