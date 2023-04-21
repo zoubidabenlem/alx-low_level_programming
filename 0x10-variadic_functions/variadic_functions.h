e473e9c64922ebe93a834f4b6c7f180de8408cba
Benlem <zoubidabenlem@gmail.com>
2023-04-20 17:14:30 -0700

    error fix

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,13 +6,13 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 /**
- * struct op - struct for 3
- * @c: character to check
+ * struct prints - struct for 3
+ * @type: character to check
  * @f: function to point to
  */
-typedef struct op
+typedef struct prints
 {
-	char *c;
-	int (*f)(va_list a);
-} printer;
+	char *type;
+	void (*f)(va_list a);
+} prints;
 #endif
