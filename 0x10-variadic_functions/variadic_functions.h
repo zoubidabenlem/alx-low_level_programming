f575a87b7f9bc0bf16d2aa8f4bd02eb57d74df61
Benlem <zoubidabenlem@gmail.com>
2023-04-20 17:24:03 -0700

    fixed problem of decalaration

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,13 +6,13 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 /**
- * struct prints - struct for 3
- * @type: character to check
+ * struct op - struct for 3
+ * @c: character to check
  * @f: function to point to
  */
 typedef struct op
 {
 	char *c;
-	void (*f)(va_list a);
+	int (*f)(va_list a);
 } printer;
 #endif
