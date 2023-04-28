9072948fe02a199981fcaa195d4e88999128d000
Benlem <zoubidabenlem@gmail.com>
2023-04-28 08:43:55 -0700

    data structs

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,7 @@
-#ifndef LISTS_H_INCLUDED
-#define LISTS_H_INCLUDED
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED #include <stddef.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h>
 /**
  * struct list_s - singly linked list
