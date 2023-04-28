77df5b9a78a429542ccb44c627bc21dcb128c687
Benlem <zoubidabenlem@gmail.com>
2023-04-28 08:51:09 -0700

    data structs

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,7 +1,8 @@
 #ifndef LIST_H_INCLUDED
-#define LIST_H_INCLUDED #include <stddef.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
+#define LIST_H_INCLUDED
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 /**
  * struct list_s - singly linked list
@@ -13,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 #endif
