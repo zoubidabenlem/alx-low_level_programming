c1091f7777df0e7ffec6bb85bd2103fca11759b5
Benlem <zoubidabenlem@gmail.com>
2023-04-28 09:27:00 -0700

    data structs

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * add_node - adds node at the beginning of list
+ * @head: head of list
+ * @str: string to b added 
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
