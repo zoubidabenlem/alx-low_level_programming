55d39840c9b120019eecd9b48d9d48724b63c1c8
Benlem <zoubidabenlem@gmail.com>
2023-04-28 06:50:36 -0700

    data structs

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_list - Prints list
+ * @h: head of the list
+ *
+ * Return: number of elements
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int i;
+
+	while(h != NULL)
+	{
+		if(h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h_len ,h->str);
+		i++;
+		head = head->next;
+	}
+	return (i);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,22 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+list_t print_list(const list_t *h);
+#endif
