25905ab27b2d5ba840027ede2a378349b19d9652
Benlem <zoubidabenlem@gmail.com>
2023-04-28 08:16:59 -0700

    data structs

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,22 +1,22 @@
 #include "lists.h"
 /**
- * print_list - Prints list
- * @h: head of the list
+ * print_list - prints all the elements
+ * @h: head of list
  *
- * Return: number of elements
+ * Return: number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	while(h != NULL)
+	while (h != NULL)
 	{
-		if(h->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n",h->len, h->str);
-		h = h->next;
+			printf("[%d] %s\n", h->len, h->str);
 		i++;
+		h = h->next;
 	}
 	return (i);
 }
