701e7716c95e1ebb52bb0543912beee01ad47733
Benlem <zoubidabenlem@gmail.com>
2023-04-29 07:21:38 -0700

    more data structs

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_listint - prints list of numbers
+ * @h: head of list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	if (h == NULL)
+		return (NULL);
+
+	while (h != NULL)
+	{
+		printf("%d\n");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
