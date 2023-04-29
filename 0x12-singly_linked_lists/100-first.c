99d19a3edb823488ac61e36e3fb099184267308a
Benlem <zoubidabenlem@gmail.com>
2023-04-28 18:00:36 -0700

    data structs

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * prints - prints before main
+ *
+ * Return: void
+ */
+
+void prints(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
