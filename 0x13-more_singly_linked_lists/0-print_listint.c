f417e2dbe28904924935e1d953e6f65e48f0a0ec
Benlem <zoubidabenlem@gmail.com>
2023-04-29 07:25:01 -0700

    more data structs

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 	size_t i = 0;
 
 	if (h == NULL)
-		return (NULL);
+		return (-1);
 
 	while (h != NULL)
 	{
-		printf("%d\n");
+		printf("%d\n", h->n);
 		h = h->next;
 		i++;
 	}
