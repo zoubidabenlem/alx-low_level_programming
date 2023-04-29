dfdef93848b2e69933d58f79b19ed35a1d89e335
Benlem <zoubidabenlem@gmail.com>
2023-04-29 15:05:32 -0700

    more data structs

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,8 +14,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	node->n = n;
 	node->next = NULL;
+	
+	if (node == NULL)
+		return (NULL);
 
-	if (*head == NULL)
+	if ((*head) == NULL)
 	{
 		*head = node;
 	}
