c1e2128612efea546f1b340ba2fedbf9c9bdd757
Benlem <zoubidabenlem@gmail.com>
2023-04-29 16:02:42 -0700

    more data structs

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_listint - Frees list
+ * @head: head of list
+ *
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t del;
+
+	while (head != NULL)
+	{
+		del = head;
+		free(del);
+		head = del->next;
+	}
+}
