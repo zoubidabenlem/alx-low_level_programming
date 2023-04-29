3c119a0e37ea3528845d10d255d95e73d3f6c058
Benlem <zoubidabenlem@gmail.com>
2023-04-29 12:07:02 -0700

    more data structs

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - Adds node at the end of list
+ * @h: head of list
+ * @n: int to be added
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *pos = malloc(sizeof(listint_t));
+	listint_t *node = malloc(sizeof(listint_t));
+	
+	node->n = n;
+	node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = node;
+	}
+	else
+	{
+		pos = *head;
+		while (pos->next != NULL)
+		{
+			pos = pos->next;
+		}
+		pos->next = node;
+	}
+	return (node);
+}
