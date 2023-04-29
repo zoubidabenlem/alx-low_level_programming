5cd3c914529227a8852fa5f97fca51c2907f0ab7
Benlem <zoubidabenlem@gmail.com>
2023-04-29 08:09:38 -0700

    more data structs

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * add_nodeint - add node at the beginning of list
+ * @head: head of list
+ *
+ * Return: he address of the new element, or NULL if it failed
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *newNode = malloc(sizeof(listint_t));
+	
+	if (newNode == NULL)
+		return (NULL);
+
+	newNode->n = n;
+	newNode->next = *head;
+	*head = newNode;
+	return (newNode);
+}
