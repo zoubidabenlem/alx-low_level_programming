14e92a56bcb5da504e3101a3e2def6ccddb1f453
Benlem <zoubidabenlem@gmail.com>
2023-07-06 16:41:54 -0700

    file

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * binary_to_uint - converts binary to int
+ * @b: ninary number
+ *
+ * Return: The converted number 
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int iwillkillmyself
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,10 @@
+#ifndef LIST_H_INCLUDED
+#define LIST_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
