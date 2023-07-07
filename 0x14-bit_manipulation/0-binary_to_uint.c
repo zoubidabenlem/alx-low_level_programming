7776c92543b478127d9486e0d130b92db9b55ed7
Benlem <zoubidabenlem@gmail.com>
2023-07-07 07:08:58 -0700

    testing func

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,10 +3,39 @@
  * binary_to_uint - converts binary to int
  * @b: ninary number
  *
- * Return: The converted number 
+ * Return: The converted number
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int iwillkillmyself
+	unsigned int num = 0;
+	int i = 0;
+	int lenght = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' || b[i] != '1')
+		{
+			return (0);
+		}
+		length++;
+	}
+
+	for (i = 0; i < length; i++)
+	{
+		if (b[i] == '0')
+		{
+			continue;
+		}
+		if (b[i] == '1')
+		{
+			num += pow(2, i);
+		}
+	}
+	return (num);
 }
