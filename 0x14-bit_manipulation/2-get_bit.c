8dfb8cf401a7293de22b731b04c71e00d44b26ba
Benlem <zoubidabenlem@gmail.com>
2023-07-07 18:47:56 -0700

    requirement fix

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned int i;
 
+	if (index > 32)
+		return (-1);
+
 	for (i = 0; i <= index; i++)
 	{
 		unsigned long int mask = 1UL << i;
