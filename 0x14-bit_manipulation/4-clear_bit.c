3c96c6ae9c165016ff925d94ec899e6772cc1671
Benlem <zoubidabenlem@gmail.com>
2023-07-07 18:46:16 -0700

    requirement fix

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -11,6 +11,9 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned int i;
 
+	if (index > 32)
+		return (-1);
+
 	for (i = 0; i <= index; i++)
 	{
 		unsigned long int masky = 1UL << i;
