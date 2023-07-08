27128944f1c921085a16fea78038833b7b145d6b
Benlem <zoubidabenlem@gmail.com>
2023-07-07 17:57:03 -0700

    function bit with tests

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * get_bit - ges bit at specified index
+ * @n: integer to use
+ * @index: index of bit
+ *
+ * Return: bit at index
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+
+	for (i = 0; i <= index; i++)
+	{
+		unsigned long int mask = 1UL << i;
+
+		if (i == index)
+		{
+			if (n & mask)
+			{
+				return (1);
+			}
+			else
+			{
+				return (0);
+			}
+		}
+	}
+	return (-1);
+}
