8b9fb4d9c3dabdad98b4e1a8f83c9c4da34e69c5
Benlem <zoubidabenlem@gmail.com>
2024-02-08 08:51:56 -0800

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,54 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: string containing the binary number
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: pointer to the string contaning the binary number
+ *
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int decimal = 0;
+	size_t ptrLen = len(b);
+	size_t i;
+	unsigned int res = 0;
 
-	if (!b)
+	if (ptrLen == 0)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; i < ptrLen; i++)
 	{
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		decimal = 2 * decimal + (b[i] - '0');
+		if (b[i] == '1')
+		{
+			res += 1 << (ptrLen - 1 - i);
+		}
 	}
-	return (decimal);
+
+	return (res);
+}
+
+/**
+ * len - calculates the length of a pointer to char
+ * @b: pointer to char
+ *
+ * Return: the length of the char pointer
+ */
+size_t len(const char *b)
+{
+	size_t i = 0;
+	const char *ptr = b;
+
+	if (b == NULL)
+		return (0);
+
+	while (*ptr != '\0')
+	{
+		if (*ptr != '0' && *ptr != '1')
+			return (0);
+		i++;
+		ptr++;
+	}
+
+	return (i);
 }
